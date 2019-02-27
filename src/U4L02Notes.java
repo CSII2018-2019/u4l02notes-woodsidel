f26b27337bc5177e7186350957d9dc2114fbcf21
lombardim <lombardim@Lombardi-12MF3Q.ellis.int>
2019-02-27 09:29:23 -0500

    U4L02 Notes

---
+++ src/U4L02Notes.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class U4L02Notes extends JFrame {
+	
+	public static final String FILE_NAME = "/zoe.jpg";
+	
+	public U4L02Notes(){
+		initGUI();
+		setTitle("Examples");
+		setResizable(true);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void initGUI(){
+		//title Panel
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(Color.BLACK);
+		JLabel titleLabel = new JLabel("Examples");
+		titleLabel.setForeground(Color.WHITE);
+		titlePanel.add(titleLabel);
+		add(titlePanel, BorderLayout.PAGE_START);
+
+
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		} catch (Exception e){}
+		
+		SwingUtilities.invokeLater(new Runnable (){
+            @Override
+            public void run() {
+                new U4L02Notes();
+            }   
+        });
+
+	}
+
+}

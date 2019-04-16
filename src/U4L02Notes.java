58d86ab08222a8b8e8d4b991d935067e8b22ba4c
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2019-04-16 10:19:56 -0400

    FINISHED

--- src/U4L02Notes.java
+++ src/U4L02Notes.java
@@ -12,66 +12,66 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 public class U4L02Notes extends JFrame {
-	
+
 	public static final String FILE_NAME = "/Spider-Man.jpg";
-	
-	public U4L02Notes(){
+
+	public U4L02Notes() {
 		initGUI();
-		setTitle("Examples");
+		setTitle("Filters");
 		setResizable(true);
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	public void initGUI(){
-		String type = JOptionPane.showInputDialog("What Filter Do You Want? \nFilters: Grayscale, Avengers, Negative, Sepia");
-		if(type == "Grayscale") {
-		//grayscale image panel
-		ImagePanel grayscale = new ImagePanel(FILE_NAME);
-		grayscale.convertToGrayscale(type);
-		add(grayscale, BorderLayout.CENTER);
+
+	public void initGUI() {
+		String type = JOptionPane.showInputDialog("What Filter Do You Want? \nFilters: Normal, Grayscale, Avengers, Negative, Sepia");
+		if (type.equals("Grayscale")) {
+			//grayscale image panel
+			type = "Grayscale";
+			ImagePanel grayscale = new ImagePanel(FILE_NAME);
+			grayscale.convertToGrayscale(type);
+			add(grayscale, BorderLayout.CENTER);
+		} else if (type.equals("Avengers")) {
+			//avengers image panel
+			type = "Avengers";
+			ImagePanel Avengers = new ImagePanel(FILE_NAME);
+			Avengers.convertToAvengers(type);
+			add(Avengers, BorderLayout.CENTER);
+		} else if (type.equals("Negative")) {
+			// negative image panel
+			type = "Negative";
+			ImagePanel Negative = new ImagePanel(FILE_NAME);
+			Negative.convertToNegative(type);
+			add(Negative, BorderLayout.CENTER);
+		} else if (type.equals("Sepia")) {
+			// sepia image panel
+			type = "Sepia";
+			ImagePanel Sepia = new ImagePanel(FILE_NAME);
+			Sepia.convertToSepia(type);
+			add(Sepia, BorderLayout.CENTER);
+		} else if(type.equals("Normal")) {
+			// normal image panel
+			type = "Normal";
+			ImagePanel imgPanel = new ImagePanel(FILE_NAME);
+			add(imgPanel, BorderLayout.CENTER);
 		}
-		else if(type == "Avengers") {
-		//avengers image panel
-				ImagePanel Avengers = new ImagePanel(FILE_NAME);
-				Avengers.convertToAvengers(type);
-				add(Avengers, BorderLayout.CENTER);
-		}
-		else if(type == "Negative") {
-		//negative image panel
-				ImagePanel Negative = new ImagePanel(FILE_NAME);
-				Negative.convertToNegative(type);
-				add(Negative, BorderLayout.CENTER);
-		}
-		else if(type == "Sepia") {
-		//sepia image panel
-				ImagePanel Sepia = new ImagePanel(FILE_NAME);
-				Sepia.convertToSepia(type);
-				add(Sepia, BorderLayout.CENTER);		
-		}
-		else {
-		//normal image panel
-		ImagePanel imgPanel = new ImagePanel(FILE_NAME);
-		add(imgPanel, BorderLayout.CENTER);
-		}
-		}
-		
-	
+	}
 
 	public static void main(String[] args) {
 		try {
 			String className = UIManager.getCrossPlatformLookAndFeelClassName();
 			UIManager.setLookAndFeel(className);
-		} catch (Exception e){}
-		
-		SwingUtilities.invokeLater(new Runnable (){
-            @Override
-            public void run() {
-                new U4L02Notes();
-            }   
-        });
+		} catch (Exception e) {
+		}
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new U4L02Notes();
+			}
+		});
 
 	}
 
